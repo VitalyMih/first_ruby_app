b4514ae12afa8e145c28c9ec24e38011b4ed4a15
VitalyMih <vitalymihale@yandex.ru>
2023-08-01 10:16:31 +0300

    Generate Article controller + Article model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'articles#index'
+
+  # Метод GET у action index в ArticlesController
+  get '/articles', to: 'articles#index'
 
   # Defines the root path route ("/")
   # root "articles#index"
